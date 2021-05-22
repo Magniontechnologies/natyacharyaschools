ea795642953406bcad667f240c679073e2e6d5a2
Magniontechnologies <54429094+Magniontechnologies@users.noreply.github.com>
2021-05-22 11:51:12 +0530

    update by k.mohamed hasim
    update by k.mohamed hasim

---
+++ js/videos/mohamedhasim-video-courses.js
@@ -0,0 +1,64 @@
+// api url
+const api_url =
+	"https://bookstoremagapi.herokuapp.com/videos/get";
+
+ 
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data1 = await response.json();
+   // console.log(data1.data[0].videos);
+//	let data = data1.data[0].videos;
+	let data = data1.data[0].videos;
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+
+
+// Function to hide the loader
+function hideloader() {
+	document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+	let tab =
+		`<tr>
+		
+		</tr>`;
+	
+		console.log(data);
+	// Loop to access all rows
+	for (let r of data) {
+		if(r.name == "courses"){
+	//	tab += r.name+'<br>'+r.location;
+//     tab +=`<tr>
+// 	<td>${r.name} </td>
+// 	<td>${r.location}</td>	
+// </tr>`
+
+    tab += `
+    <iframe src="${r.location}"
+ frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+  encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+  </iframe>
+    `
+
+
+ 
+
+		}
+	}
+
+    
+
+	// Setting innerHTML as tab variable
+	document.getElementById("employees").innerHTML = tab;
+}
